fd59b013df2cbcfac633a5c9c9fcaa595c8f3c6c
Alexandre Andrade de Avila <alexandre_avila@virtualloja.com.br>
2019-05-17 16:45:19 -0300

    asdf

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/VirtualWeb-App'));
 
-// Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/VirtualWeb-App/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
