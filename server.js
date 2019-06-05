698fcb754e3c185141c660d9c6c2e41fe22414cf
Alexandre Andrade de Avila <alexandre_avila@virtualloja.com.br>
2019-06-05 08:47:32 -0300

    web socket 0.2

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const app = express();
 
-app.set('origins', '*:*');
+const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/VirtualWeb-App'));
@@ -11,9 +10,9 @@ app.use(express.static(__dirname + '/dist/VirtualWeb-App'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/VirtualWeb-App/index.html'));
 });
- 
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
 let http = require('http');
 let server = http.Server(app);
@@ -33,8 +32,6 @@ io.on('connection', (socket) => {
 
     socket.on('StatusRetornoAlterado', (dados) => { io.emit('StatusRetornoAlterado', dados); });
 });
-
-
 
 server.listen(port, () => {
 
