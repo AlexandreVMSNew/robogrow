5df9a1bc50fa5e4fe20537703929993186be83a7
Alexandre Andrade de Avila <alexandre_avila@virtualloja.com.br>
2019-06-05 09:01:49 -0300

    web socket 0.4

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+let http = require('ws').Server;
+let server = http.Server(app);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/VirtualWeb-App'));
 
@@ -15,3 +18,28 @@ app.get('/*', function(req, res) {
 app.listen(process.env.PORT || 8080);
 
 
+let socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = new SocketServer({ server });
+
+io.on('connection', (socket) => {
+
+    socket.on('NovoRetorno', (dados) => { io.emit('NovoRetorno', dados); });
+
+    socket.on('NovaObservacao', (dados) => { io.emit('NovaObservacao', dados); });
+
+    socket.on('NotificacaoUsuarioRetorno', (dados) => { io.emit('NotificacaoUsuarioRetorno', dados); });
+
+    socket.on('StatusRetornoAlterado', (dados) => { io.emit('StatusRetornoAlterado', dados); });
+});
+
+server.listen(port, () => {
+
+});
