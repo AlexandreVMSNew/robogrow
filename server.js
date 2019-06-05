584c1678bf63403feda6bca117c224b420953dfe
Alexandre Andrade de Avila <alexandre_avila@virtualloja.com.br>
2019-06-05 08:53:17 -0300

    web socket 0.3

--- server.js
+++ server.js
@@ -12,27 +12,6 @@ app.get('/*', function(req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
-let http = require('http');
-let server = http.Server(app);
 
-let socketIO = require('socket.io');
-let io = socketIO(server);
-
-const port = process.env.PORT || 3000;
-
-io.on('connection', (socket) => {
-
-    socket.on('NovoRetorno', (dados) => { io.emit('NovoRetorno', dados); });
-
-    socket.on('NovaObservacao', (dados) => { io.emit('NovaObservacao', dados); });
-
-    socket.on('NotificacaoUsuarioRetorno', (dados) => { io.emit('NotificacaoUsuarioRetorno', dados); });
-
-    socket.on('StatusRetornoAlterado', (dados) => { io.emit('StatusRetornoAlterado', dados); });
-});
-
-server.listen(port, () => {
-
-});
